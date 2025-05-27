9d97ddefd84efe10a65a39a08aa744533a7cede5
Singh <narender.singh@harman.com>
2025-05-27 11:31:57 +0530

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
+const getFingerprint = async () => {
+  const fp = await FingerprintJS.load();
+  const result = await fp.get();
+  //console.log('Fingerprint:', result.visitorId);
+  return result.visitorId;
+};
+
+const deviceId=getFingerprint();
+
+
+
+
+
+
+
+
+
+class Counter extends Component {
+    state = { 
+        
+     } 
+
+     
+  
+
+
+    render() { 
+       
+
+        return (
+            <div className="container">
+                <h1>Device ID is: {deviceId}</h1>                
+            </div>
+        );
+    }
+}
+ 
+export default Counter;
